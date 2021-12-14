f6dbccd726f43c09fbe28fa7ec7f82caffe70830
gsimpson <gsimpson@pobox.com>
2021-12-14 11:43:50 -0700

    flask example working for view home and about

---
+++ flask-logrocket/flask_docker/hello.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+# https://stackoverflow.com/questions/23327293/flask-raises-templatenotfound-error-even-though-template-file-exists
+
+app=Flask(__name__,template_folder='template')
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/about/")
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
