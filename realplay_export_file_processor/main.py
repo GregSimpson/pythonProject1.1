1c884130f7494e747af82f0f07115f2e536397da
gsimpson <gsimpson@pobox.com>
2021-12-23 17:03:26 -0700

    need superuser ids

--- realplay_export_file_processor/main.py
+++ realplay_export_file_processor/main.py
@@ -113,18 +113,28 @@ def process_input_files(json_or_csv):
                 process_this_df(this_df, upload_filename)
 
 
-def connect_to_db(postgres_hostname,postgres_ip,postgres_port,postgres_host,postgres_db_name,postgres_user,postgres_pswd):
+#def connect_to_db(postgres_hostname,postgres_ip,postgres_port,postgres_host,postgres_db_name,postgres_user,postgres_pswd):
+def connect_to_db(postgres_hostname, postgres_port, postgres_host, postgres_db_name, postgres_user,
+                      postgres_pswd):
     logger.debug("establishing the db connection to: {}".format(postgres_hostname))
     # establishing the connection
     conn = psycopg2.connect(
         database=postgres_db_name
         , user=postgres_user
         , password=postgres_pswd
-        , host=postgres_hostname
+        , host=postgres_host
         , port=postgres_port
     )
     logger.debug("connected to a db (I think) ")
     return conn
+
+
+def run_a_db_query(db_conn, my_stmt):
+    cur = db_conn.cursor()
+    cur.execute(my_stmt)
+    items = cur.fetchall()
+    return items
+        #logger.debug(items)
 
 
 def process_upload_files():
@@ -138,7 +148,7 @@ def process_upload_files():
     logger.debug('postgres_d : {}'.format(postgres_envs))
 
     postgres_hostname = " "
-    postgres_ip = " "
+    #postgres_ip = " "
     postgres_port = " "
     postgres_host = " "
     postgres_db_name = " "
@@ -150,7 +160,7 @@ def process_upload_files():
         if (environment.lower() == 'dev'):
             logger.debug('found DEV env : {}'.format(environment))
             postgres_hostname = parser.get('DEV_DB', 'hostname', fallback=" ")
-            postgres_ip = parser.get('DEV_DB', 'private ip', fallback=" ")
+            #postgres_ip = parser.get('DEV_DB', 'private ip', fallback=" ")
             postgres_port = parser.get('DEV_DB', 'port', fallback=" ")
             postgres_host = parser.get('DEV_DB', 'host', fallback=" ")
             postgres_db_name = parser.get('DEV_DB', 'db_name', fallback=" ")
@@ -160,7 +170,7 @@ def process_upload_files():
         elif (environment.lower() == 'qa'):
             logger.debug('found QA env : {}'.format(environment))
             postgres_hostname = parser.get('QA_DB', 'hostname', fallback=" ")
-            postgres_ip = parser.get('QA_DB', 'private ip', fallback=" ")
+            #postgres_ip = parser.get('QA_DB', 'private ip', fallback=" ")
             postgres_port = parser.get('QA_DB', 'port', fallback=" ")
             postgres_host = parser.get('QA_DB', 'host', fallback=" ")
             postgres_db_name = parser.get('QA_DB', 'db_name', fallback=" ")
@@ -170,7 +180,7 @@ def process_upload_files():
         elif (environment.lower() == 'prod'):
             logger.debug('found PROD env : {}'.format(environment))
             postgres_hostname = parser.get('PROD_DB', 'hostname', fallback=" ")
-            postgres_ip = parser.get('PROD_DB', 'private ip', fallback=" ")
+            #postgres_ip = parser.get('PROD_DB', 'private ip', fallback=" ")
             postgres_port = parser.get('PROD_DB', 'port', fallback=" ")
             postgres_host = parser.get('PROD_DB', 'host', fallback=" ")
             postgres_db_name = parser.get('PROD_DB', 'db_name', fallback=" ")
@@ -181,23 +191,105 @@ def process_upload_files():
             logger.error('postgres database info not found - exiting')
             exit(5)  # just made up a number
 
+        db_conn = connect_to_db(
+            postgres_hostname,
+            #postgres_ip,
+            postgres_port,
+            postgres_host,
+            postgres_db_name,
+            postgres_user,
+            postgres_pswd
+        )
+
+        cur = db_conn.cursor()
+        my_stmt = "select current_database();"
+        cur.execute(my_stmt)
+        items = cur.fetchall()
+        logger.debug(items)
+
+        # my_stmt = "ALTER USER {} WITH SUPERUSER;".format(postgres_user)
+        my_stmt = "SELECT usename AS role_name, CASE WHEN usesuper AND usecreatedb THEN CAST('superuser, create database' AS pg_catalog.text) WHEN usesuper THEN CAST('superuser' AS pg_catalog.text) WHEN usecreatedb THEN CAST('create database' AS pg_catalog.text) ELSE CAST('' AS pg_catalog.text) END role_attributes FROM pg_catalog.pg_user ORDER BY role_name desc;"
+        logger.debug(my_stmt)
+        cur.execute(my_stmt)
+        items = cur.fetchall()
+        logger.debug(items)
+
+        #  https://www.oodlestechnologies.com/blogs/Postgres-Sql-Update-Record-in-Bulk-from-CSV-file/
 
         # r=root, d=directories, f = files
         for r, d, f in walklevel(upload_source_dir, 0):
             for file in f:
-                db_conn = connect_to_db(
-                    postgres_hostname,
-                    postgres_ip,
-                    postgres_port,
-                    postgres_host,
-                    postgres_db_name,
-                    postgres_user,
-                    postgres_pswd
-                )
+
 
                 if file.endswith('.csv'):
-                    logger.debug('upload file:\n\t{}\n\tto {}'.format(file, postgres_db_name))
+                    full_path = os.path.join(r, file)
 
+                    logger.debug('upload file:\n\t{}\n\tto {}'.format(full_path, postgres_db_name))
+                    my_stmt = "create temporary table gjs_test (user_id character varying(255) , email character varying(255) , roles character varying(255) );"
+                    #cur.execute(my_stmt)
+
+                    # this should work IF you get a superuser if
+                    my_stmt = "COPY gjs_test FROM '{}' WITH (FORMAT csv);".format(full_path)
+                    logger.debug("\t\t running this stmt\n{}".format(my_stmt))
+                    #cur.execute(my_stmt)
+
+                    # once that loads, run something like this to update from the new temp table:
+                    # UPDATE original_table SET original_table.dob = temp_table.dob FROM temp_table  Where original_table.id = temp_table.id;
+
+                    my_stmt = "select * from gjs_test;"
+                    #cur.execute(my_stmt)
+                    #items = cur.fetchall()
+                    #logger.debug(items)
+
+                    my_stmt = "drop table gjs_test;"
+                    #cur.execute(my_stmt)
+
+
+def gjs_junk_holeder():
+    logger.debug('just stuff')
+    '''
+    my_stmt = "create temporary table gjs_test (user_id character varying(255) , email character varying(255) , roles character varying(255) );"
+    cur.execute(my_stmt)
+
+    my_stmt = "copy public.gjs_test <filename> delimiter ',' csv noheader>;"
+    cur.execute(my_stmt)
+
+    my_stmt = "select * from gjs_test;"
+    cur.execute(my_stmt)
+    items = cur.fetchall()
+    logger.debug(items)
+    '''
+
+    # my_stmt = "create temporary table gjs_test as select * from realplay_user;"
+    # my_stmt = "create table public.gjs_test (user_id character varying(255) , email character varying(255) , roles character varying(255) );"
+    ###my_stmt = "create  table public.gjs_realplay_user as select * from realplay_user;"
+    ###cur.execute(my_stmt)
+
+    # my_stmt = "select * from  gjs_realplay_user;"
+    # cur.execute(my_stmt)
+    # items = cur.fetchall()
+    # logger.debug(items)
+
+    # my_stmt = "create  table public.gjs_test as select realplay_user.userid , realplay_user.email , realplay_user.tenant from realplay_user;"
+    # cur.execute(my_stmt)
+
+    # my_stmt = "select * from  gjs_test;"
+    # cur.execute(my_stmt)
+    # items = cur.fetchall()
+    # logger.debug(items)
+
+    ##my_stmt = "select nspname from pg_namespace where oid  =  pg_my_temp_schema();"
+    ##cur.execute(my_stmt)
+    ##items = cur.fetchall()
+    ##logger.debug(items)
+
+    # my_stmt = "drop table gjs_test;"
+    # cur.execute(my_stmt)
+
+    # my_stmt = "select * from  gjs_test;"
+    # cur.execute(my_stmt)
+    # items = cur.fetchall()
+    # logger.debug(items)
 
 
 if __name__ == '__main__':
