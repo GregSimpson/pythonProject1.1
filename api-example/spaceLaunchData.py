8be0cdd0ceae28b61ac0e4c866a40739839d7a31
U-DESKTOP-H1BDSCN\gsimp <gsimpson@pobox.com>
2022-07-29 12:31:06 -0600

    all gets work

--- api-example/spaceLaunchData.py
+++ api-example/spaceLaunchData.py
@@ -13,32 +13,91 @@ f2 = r"https://launchlibrary.net/1.3/launch/2019-11-01"
 # https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/manager/api/v1/agents
 #
 
-# UJET test
-UJET_API = "https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/manager/api/v1/agents"
-URL = UJET_API
-USERNAME = "GregSimpson"
-PASSWORD = "<api key pswd goes here>"
 
+# UJET test apis
+USERNAME = "GregSimpson"
+PASSWORD = "<api pswd here>"
 PARAMS = {'Username': USERNAME, 'Password' : PASSWORD}
 HEADERS = {'Accept': 'application/vnd.json', 'User-Agent': "tap-ujet greg.simpson@ttec.com", 'Content-Type': 'application/json'}
 METHOD = 'GET'
 
+# GET APIs
+# Manager APIs
+UJET_BASE = "https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/manager/api/v1/"
+UJET_API = "agents"
+UJET_API = "agent_activity_logs"
+UJET_API = "agents/current_status"
+UJET_API = "calls"
+UJET_API = "chats"
+UJET_API = "user_statuses"
+UJET_API = "teams"
 
-'''
-response = self.__session.request(
-                method=method,
-                url=url,
-                auth=(self.__company_key, self.__company_secret),
-                json=json,
-                **kwargs)
-'''
+UJET_API = "teams/1"
+UJET_API = "teams/tree"
 
+# APPS APIs
+UJET_BASE = "https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/apps/api/v1/"
+UJET_API = "wait_times?lang=en"
+
+URL = UJET_BASE + UJET_API
+
+
+HEADERS = { \
+    'Content-Length': '0', \
+    'User-Agent': "tap-ujet greg.simpson@ttec.com", \
+    'Accept': '*/*', \
+    'Accept-Encoding': 'gzip, deflate, br', \
+    'Connection': 'keep-alive' \
+    }
+
+print(URL)
+print(USERNAME)
+print(PASSWORD)
+print(HEADERS)
 
 g = requests.get(
     url=URL,
     auth=(USERNAME, PASSWORD),
-    json=None)
+    json=None,
+    headers=HEADERS)
 print(g.json())
+
+
+# PUT APIs
+UJET_BASE = "https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/apps/api/v1/"
+
+# Outbound call
+UJET_API = "calls?lang=en"
+# Incoming Call
+#UJET_API = "calls?lang=en"
+# Outbound SMS
+#UJET_API = "calls"
+# CRM Contact Data
+#UJET_API = "crm_contact_cdata?lang=en"
+
+#URL = UJET_BASE + UJET_API = "https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/apps/api/v1/calls?lang=en"
+URL = UJET_BASE + UJET_API
+
+HEADERS = { \
+    'Content-Length': '0', \
+    'User-Agent': "PostmanRuntime/7.29.2", \
+    'Accept': '*/*', \
+    'Accept-Encoding': 'gzip, deflate, br', \
+    'Connection': 'keep-alive' \
+    }
+
+print(URL)
+print(USERNAME)
+print(PASSWORD)
+print(HEADERS)
+
+g = requests.put(
+    url=URL,
+    auth=(USERNAME, PASSWORD),
+    json=None,
+    headers=HEADERS
+)
+print(g)
 
 #r = requests.get(url=UJET_API, params=PARAMS, headers=HEADERS)
 #print(r.json())
