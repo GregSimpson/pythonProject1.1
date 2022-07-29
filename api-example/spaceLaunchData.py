7d1756407e9d2d83f7270be71ce432d113b4dea6
U-DESKTOP-H1BDSCN\gsimp <gsimpson@pobox.com>
2022-07-29 11:45:43 -0600

    get works

--- api-example/spaceLaunchData.py
+++ api-example/spaceLaunchData.py
@@ -2,8 +2,9 @@
 
 # this api call works
 import requests
+
 f2 = r"https://launchlibrary.net/1.3/launch/2019-11-01"
-print(requests.get(f2).text)
+#print(requests.get(f2).text)
 
 
 #  I want to do something similar, but call to Ujet's api.
@@ -14,9 +15,30 @@ print(requests.get(f2).text)
 
 # UJET test
 UJET_API = "https://ttecphoenix-5x4khsz.uc1.ccaiplatform.com/manager/api/v1/agents"
+URL = UJET_API
 USERNAME = "GregSimpson"
-PASSWORD = "<password here>"
+PASSWORD = "<api key pswd goes here>"
 
 PARAMS = {'Username': USERNAME, 'Password' : PASSWORD}
-r = requests.get(url=UJET_API, params=PARAMS)
-print(r.json())
+HEADERS = {'Accept': 'application/vnd.json', 'User-Agent': "tap-ujet greg.simpson@ttec.com", 'Content-Type': 'application/json'}
+METHOD = 'GET'
+
+
+'''
+response = self.__session.request(
+                method=method,
+                url=url,
+                auth=(self.__company_key, self.__company_secret),
+                json=json,
+                **kwargs)
+'''
+
+
+g = requests.get(
+    url=URL,
+    auth=(USERNAME, PASSWORD),
+    json=None)
+print(g.json())
+
+#r = requests.get(url=UJET_API, params=PARAMS, headers=HEADERS)
+#print(r.json())
